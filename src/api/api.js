8aa02eab3897488b705eb04b04946cb63085bf5f
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2024-10-19 18:41:50 +0530

    Serverless fetching added

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const isDevelopment = import.meta.env.DEV;
 const api_key = import.meta.env.VITE_API_KEY;
 
 
-const baseURL = isDevelopment ? "/api/maps/place/nearbysearch/json" : "/api/getNearbyRestaurant";
+const baseURL = isDevelopment ? "/api/maps/place/nearbysearch/json" : "/api/getNearbyRestaurants";
 console.log( baseURL)
 const GoogleMapsApi = axios.create({
   baseURL,
